1202a5c733756c188ab5faa71d9153b6ebfcdfb4
iam-kevin <kevin.al.james@gmail.com>
2022-02-28 14:36:36 +0300

    remove `@elsa-ui/react-native` [was placeholder]

--- packages/ui-react-native/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import { Label } from ".";
-
-describe("Sample testing", () => {
-	it("Component works", () => {
-		render(<Label>Testing!</Label>);
-	});
-});
--- packages/ui-react-native/jest.config.js
+++
@@ -1,5 +0,0 @@
-// jest.config.js
-
-module.exports = {
-  preset: "react-native",
-};
