81195955921f33a31542df6df22c95391021e904
iam-kevin <kevin.al.james@gmail.com>
2022-03-19 23:35:49 +0300

    bump compileSdkVersion for mobile-providers

--- packages/data-fns/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { n as nextSteps } from './nextSteps-8a6f3466';
-export { LabTest, Medication, labTests, medications } from './recommended';
-export { Condition, Symptom, SymptomDescription, conditions, donparMap, symptoms, symptomsLocale } from './base';
-export { Investigation, InvestigationType, InvestigationTypeRecord, investigation } from './investigations';
