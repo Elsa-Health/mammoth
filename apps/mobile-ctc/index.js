880fa7f6e44f912c07c21d96a39ce4e6f8ab2281
iam-kevin <kevin.al.james@gmail.com>
2022-04-14 12:36:19 +0300

    making code push work

--- apps/mobile-ctc/index.js
+++ apps/mobile-ctc/index.js
@@ -7,4 +7,24 @@ import App from './src/_app';
 import {name as appName} from './app.json';
 
 import 'react-native-gesture-handler';
-AppRegistry.registerComponent(appName, () => App);
+import codePush from 'react-native-code-push';
+import * as Sentry from '@sentry/react-native';
+
+// FIXME: Remove API key and secret
+Sentry.init({
+  dsn: 'https://6ca7254d249c4739b3db2cb7af62b796@o683972.ingest.sentry.io/5804165',
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  // tracesSampleRate: 1.0,
+  tracesSampleRate: 0.5,
+});
+
+const FinalApp = Sentry.wrap(
+  codePush({
+    // updateDialog: true,
+    checkFrequency: codePush.CheckFrequency.ON_APP_START,
+    installMode: codePush.InstallMode.IMMEDIATE,
+  })(App),
+);
+
+AppRegistry.registerComponent(appName, () => FinalApp);
