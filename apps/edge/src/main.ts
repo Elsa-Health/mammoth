10b2597d04a901f350a38cfe6874b3c75a239d13
iam-kevin <kevin.al.james@gmail.com>
2022-04-26 00:12:52 +0300

    add `edge` - nestjs init

---
+++ apps/edge/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(env.PORT || 3000);
+}
+bootstrap();
