5bcf1f1f038f79a21eb1fb240f558f0505bd99b6
Kevin James <kevin.al.james@gmail.com>
2022-07-15 09:09:59 +0300

    fix: react-native-reanimated#17

--- apps/mobile-ctc/android/app/src/main/java/health/elsa/ctc/MainActivity.java
+++ apps/mobile-ctc/android/app/src/main/java/health/elsa/ctc/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
       SplashScreen.show(this);  // here
-      super.onCreate(savedInstanceState);
+      super.onCreate(null);
   }
 
   /**
